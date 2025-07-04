e433e798733313e924af9c62dc5ba77a078b9192
elvinaangga <elvina.angga30@gmail.com>
2025-07-04 18:28:43 +0900

    Completed task

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -1,10 +1,7 @@
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.List;
 import static org.junit.Assert.*;
 
 public class   UserServiceTest {
